606ed62eb6076641d90d5a2c07b22b56ce694a40
Philip Huff <philiphuff7@gmail.com>
2019-11-02 11:54:50 +0000

    first commit

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://get.docker.com/ | sh
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0-rc4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
