25ec2868bc2844a11324bf4152a59b5389b7d581
James <jspbromley@gmail.com>
2021-12-02 13:21:12 -0600

    Adding render template and button to save file.

--- docker/app.py
+++ docker/app.py
@@ -1,11 +1,19 @@
 # flask_web/app.py
 from flask import Flask
+from flask import render_template
+from flask.globals import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    if request.method == "POST":
+        my_file = open("test.txt", "w+")
+        my_file.write("Test text written to the file")
+        my_file.close()
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
