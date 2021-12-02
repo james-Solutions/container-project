b846acaa5fe8aa3f08804d107cb6ee3eb8e1c121
James <jspbromley@gmail.com>
2021-12-02 15:15:07 -0600

    Changing file save location to volume.

--- docker/app.py
+++ docker/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/', methods=["GET", "POST"])
 def hello_world():
     if request.method == "POST":
-        my_file = open("test.txt", "w+")
+        my_file = open("/storage/test.txt", "w+")
         my_file.write("Test text written to the file")
         my_file.close()
         return render_template("index.html")
